1e37937ec04a938b92001bac72f90e1f8fe92a67
Pedro Pazello(slow17) <pedropazello.rj@gmail.com>
2014-05-15 21:22:55 -0300

    one changes..

--- JNotepad/src/br/com/ppazello/view/Principal.java
+++ JNotepad/src/br/com/ppazello/view/Principal.java
@@ -4,11 +4,8 @@ import br.com.ppazello.model.Janela;
 
 public class Principal {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Janela j = new Janela();
 		j.exibir();
 	}
